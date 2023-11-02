c62343ca1da931ff887e8d9436d67aa1655d687f
Sergey Balashov <waisted.deals.07@icloud.co>
2023-11-02 13:10:10 -0700

    Initial Commit

---
+++ TraficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TraficLight
+//
+//  Created by Sergey Balashov on 11/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
