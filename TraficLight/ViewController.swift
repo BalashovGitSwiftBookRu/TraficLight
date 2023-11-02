aa3ecc19c29488b5cfc5490498b671d9b4dabc74
Sergey Balashov <waisted.deals.07@icloud.co>
2023-11-02 13:52:30 -0700

    THe base layout is created. Lights, button, color, alpha, corner radius.

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -9,8 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    
+    @IBOutlet weak var yellowView: UIView!
+    
+    @IBOutlet weak var greenView: UIView!
+    
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Округлить углы всех трех вью.
+        redView.layer.cornerRadius = 60
+        yellowView.layer.cornerRadius = 60
+        greenView.layer.cornerRadius = 60
+        
+        // Задать исходное значение alpha для всех трех вью.
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 6
+    
         
     }
 
