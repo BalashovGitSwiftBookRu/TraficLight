157932eebd7713e1dd54e5aaf24c7254b601c740
Sergey Balashov <waisted.deals.07@icloud.co>
2023-11-04 20:51:52 -0700

    Implemented functionality for switching traffic lights

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -15,28 +15,50 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var greenView: UIView!
     
-    
     @IBOutlet weak var startButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Округлить углы всех трех вью.
+        // Округлить углы вью.
         redView.layer.cornerRadius = 60
         yellowView.layer.cornerRadius = 60
         greenView.layer.cornerRadius = 60
         
-        // Задать исходное значение alpha для всех трех вью.
+        // Установить уровень прозрачности для вью.
         redView.alpha = 0.3
         yellowView.alpha = 0.3
         greenView.alpha = 0.3
         
-        startButton.layer.cornerRadius = 6
-    
-        
+        // Округлить углы кнопки.
+        startButton.layer.cornerRadius = 10
     }
+    
 
-
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        
+        // Переключение сигналов светофора.
+        if redView.alpha != 1 && yellowView.alpha != 1 && greenView.alpha != 1 {
+            redView.alpha = 1
+            
+        } else if redView.alpha == 1 && yellowView.alpha != 1 && greenView.alpha != 1 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+            
+        } else if redView.alpha != 1 && yellowView.alpha == 1 && greenView.alpha != 1 {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+            
+        } else if redView.alpha != 1 && yellowView.alpha != 1 && greenView.alpha == 1 {
+            greenView.alpha = 0.3
+            redView.alpha = 1
+        }
+        
+        // Изменение текста на кнопке.
+        if startButton.titleLabel?.text == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+    }
 }
 
